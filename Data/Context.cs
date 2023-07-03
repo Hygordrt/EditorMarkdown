dc654139ffe46bd2ea65feadf1fb0964fb95552e
Hygor <Hygor@DESKTOP-55BA59J>
2023-07-03 19:30:04 -0300

    Configuração e criação do banco sqlite.

---
+++ Data/Context.cs
@@ -0,0 +1,22 @@
+﻿using EditorMarkdown.ModelMkd;
+using Microsoft.EntityFrameworkCore;
+
+namespace EditorMarkdown.Data
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Modelmkd> Cadastro { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> opcoes) : base(opcoes)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Modelmkd>()
+                .HasKey(t => t.Id)
+                .HasAnnotation("Sqlite:Autoincrement", true);
+        }
+    }
+}
---
+++ Model/Modelmkd.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EditorMarkdown.ModelMkd
+{
+    public class Modelmkd
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string ConteudoMkd { get; set; }
+    } 
+}
