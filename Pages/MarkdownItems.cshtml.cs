03b967c9343f0976932d845c174542cdcd9175d9
Hygor <Hygor@DESKTOP-55BA59J>
2023-07-04 00:17:44 -0300

    função para salvar o conteudo do mkd no banco e atualizar

--- Pages/MarkdownItems.cshtml.cs
+++ Pages/MarkdownItems.cshtml.cs
@@ -1,19 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EditorMarkdown.Data;
+using EditorMarkdown.ModelMkd;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace EditorMarkdown.Pages
 {
     public class MarkdownItemsModel : PageModel
     {
-        private readonly ILogger<MarkdownItemsModel> _logger;
+        [BindProperty]
+        public Modelmkd cadastro { get; set; }
+        public Modelmkd CadastroMkd { get; set; }
 
-        public MarkdownItemsModel(ILogger<MarkdownItemsModel> logger)
+        private readonly ILogger<MarkdownItemsModel> _logger;
+        private readonly Contexto _contexto;
+
+        public MarkdownItemsModel(ILogger<MarkdownItemsModel> logger, Contexto contexto)
         {
+            _contexto = contexto;
             _logger = logger;
+
+            // Inicialização da propriedade CadastroMkd
+            CadastroMkd = _contexto.Cadastro.FirstOrDefault(c => c.Id == 1);
         }
 
         public void OnGet()
         {
+            CadastroMkd = _contexto.Cadastro.FirstOrDefault(c => c.Id == 1);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var registroExistente = await _contexto.Cadastro.FirstOrDefaultAsync(c => c.Id == 1);
+            if (registroExistente != null)
+            {
+                //Registro ja existe e vai ser alterado
+                registroExistente.ConteudoMkd = cadastro.ConteudoMkd;
+                await _contexto.SaveChangesAsync();
+
+            }
+            else { 
+                var cadastroMkd = new Modelmkd()
+                {
+                    Id = 01,
+                    ConteudoMkd = cadastro.ConteudoMkd
+                };
+            
+                await _contexto.Cadastro.AddAsync(cadastroMkd);
+                // Salvando
+                await _contexto.SaveChangesAsync();
+
+            }
+            return Page();
         }
     }
 }
